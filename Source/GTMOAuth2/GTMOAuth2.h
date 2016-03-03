a3d00418cfad49f40e081f4cd460d98d3f995a1d
David Gelhar <david.gelhar@gmail.com>
2016-03-02 22:26:26 -0500

    Fixes to framework build process

---
+++ Source/GTMOAuth2/GTMOAuth2.h
@@ -0,0 +1,25 @@
+//
+//  GTMOAuth2.h
+//  GTMOAuth2
+//
+//  Created by David Gelhar on 3/2/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GTMOAuth2.
+FOUNDATION_EXPORT double GTMOAuth2VersionNumber;
+
+//! Project version string for GTMOAuth2.
+FOUNDATION_EXPORT const unsigned char GTMOAuth2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMOAuth2/PublicHeader.h>
+
+
+#import <GTMOAuth2/GTMHTTPFetcher.h>
+#import <GTMOAuth2/GTMHTTPFetcherLogging.h>
+#import <GTMOAuth2/GTMHTTPFetchHistory.h>
+#import <GTMOAuth2/GTMOAuth2Authentication.h>
+#import <GTMOAuth2/GTMOAuth2Signin.h>
+#import <GTMOAuth2/GTMOAuth2WindowController.h>
